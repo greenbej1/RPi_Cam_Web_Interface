01fdd660fcb6739c0a3a6cdc840bd2d5670f043b
greenbej1 <thejohn90@gmail.com>
2016-04-27 18:27:21 -0400

    Update config.php

--- www/config.php
+++ www/config.php
@@ -18,7 +18,7 @@
    define('CAM_NAME', 'mycam');
 
    // unique camera string build from application name, camera name, host name
-   define('CAM_STRING', APP_NAME . " " . APP_VERSION . ": " . CAM_NAME . '@' . HOST_NAME);
+   
 
    // file where default settings changes are stored
    define('CONFIG_FILE1', 'raspimjpeg');
