0ffcdfa9d5838de08172e68322d0dee3ea592be4
greenbej1 <thejohn90@gmail.com>
2016-04-27 14:27:05 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -200,64 +200,10 @@
          <?php pan_controls(); ?>
          <a href="preview.php" class="btn btn-default">Download Videos and Images</a>
          &nbsp;&nbsp;
-         <?php  if($config['motion_external']): ?><a href="motion.php" class="btn btn-default">Edit motion settings</a>&nbsp;&nbsp;<?php endif; ?>
-         <a href="schedule.php" class="btn btn-default">Edit schedule settings</a>
+        
       </div>
     
       
-            <div class="panel panel-default" <?php  if($config['motion_external']) echo "style ='display:none;'"; ?>>
-               <div class="panel-heading">
-                  <h2 class="panel-title">
-                     <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Motion Settings</a>
-                  </h2>
-               </div>
-               <div id="collapseTwo" class="panel-collapse collapse">
-                  <div class="panel-body">
-                     <table class="settingsTable">
-                        <tr>
-                          <td>Motion Vector Preview:</td>
-                          <td>
-                            <select onchange="send_cmd('vp ' + this.value);setTimeout(function(){location.reload(true);}, 1000);" id="preview_select"><?php makeOptions($options_vp, 'vector_preview'); ?></select>
-                          </td>
-                        </tr>
-                        <tr>
-                           <td>Noise level (1-255):</td>
-                           <td>
-                              <?php makeInput('motion_noise', 5); ?><input type="button" value="OK" onclick="send_cmd('mn ' + document.getElementById('motion_noise').value)">
-                           </td>
-                        </tr>
-                        <tr>
-                           <td>Threshold (1-32000):</td>
-                           <td>
-                              <?php makeInput('motion_threshold', 5); ?><input type="button" value="OK" onclick="send_cmd('mt ' + document.getElementById('motion_threshold').value)">
-                           </td>
-                        </tr>
-                        <tr>
-                           <td>Mask Image:</td>
-                           <td>
-                              <?php makeInput('motion_image', 30); ?><input type="button" value="OK" onclick="send_cmd('mi ' + document.getElementById('motion_image').value)">
-                           </td>
-                        </tr>
-                        <tr>
-                           <td>Change Frames to start:</td>
-                           <td>
-                              <?php makeInput('motion_startframes', 5); ?><input type="button" value="OK" onclick="send_cmd('mb ' + document.getElementById('motion_startframes').value)">
-                           </td>
-                        </tr>
-                        <tr>
-                           <td>Still Frames to stop:</td>
-                           <td>
-                              <?php makeInput('motion_stopframes', 5); ?><input type="button" value="OK" onclick="send_cmd('me ' + document.getElementById('motion_stopframes').value)">
-                           </td>
-                        </tr>
-                        <tr>
-                           <td>Save vectors to .dat :<br>Uses more space</td>
-                           <td><select onchange="send_cmd('mf ' + this.value);"><?php makeOptions($options_mf, 'motion_file'); ?></select></td>
-                        </tr>
-                     </table>
-                  </div>
-               </div>
-            </div>
             
          </div>
       </div>
